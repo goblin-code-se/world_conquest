50e57630f172f7f4c05091b3ad224951962d82f1
fiery <fiery@pc>
2024-02-16 19:43:10 +0000

    Added debug info on hover, some update functionality for information, and texture changing dependant on the who_owns variable within a territory

--- scripts/board.gd
+++ scripts/board.gd
@@ -1,10 +1,10 @@
 class Board:
-    func _init(board_nodes: Array[Node]):
-        var nodes = board_nodes
+	func _init(board_nodes: Array[Node]):
+		var nodes = board_nodes
 
 class Territory:
-    func _init(territory_connections: Array[Node], territory_continent: Continent):
-        var connections = territory_connections
-        var continent = territory_connections
+	func _init(territory_connections: Array[Node], territory_continent: Continent):
+		var connections = territory_connections
+		var continent = territory_connections
 
 enum Continent {AFRICA, ASIA, AUSTRALIA, EUROPE, NORTH_AMERICA, SOUTH_AMERICA}

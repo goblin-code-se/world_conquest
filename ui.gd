e2175baf9daec378753fed67388f9942d9a817f4
0cr0wbar0 <finnianholland2@gmail.com>
2024-02-26 17:33:48 +0000

    Submitted basic framework for game UI

---
+++ ui.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+@export var player_count = 5
+@export var troop_count = 25
+
+func _ready():
+	$TurnTimer.start(500) # Five minutes max for a turn
+	for i in range(0, player_count):
+		$Tallies.text += "Player {num}'s troops: {troops}\n".format({"num":i, "troops":troop_count})
+
+func _process(_delta):
+	$TurnCountdown.text = str($TurnTimer.get_time_left()/60)+":"+str($TurnTimer.get_time_left()%60) # div by 60 to get minutes, mod by 60 to get mins
+
+func end_turn():
+	$TurnTimer.stop()
+
+func _on_button_pressed():
+	end_turn()

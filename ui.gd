fd65a1bfe328d280c8ab850f27f6dd3a18c95dad
0cr0wbar0 <finnianholland2@gmail.com>
2024-02-28 16:12:23 +0000

    Added small timer offset so that it starts at 5:00 and not 4:59

--- ui.gd
+++ ui.gd
@@ -4,17 +4,17 @@ extends CanvasLayer
 @export var troop_count = 25
 
 func _ready():
-	$TurnTimer.start(5*60) # Five minutes max for a turn
+	$TurnTimer.start(5*60+0.5) # Five minutes max for a turn, with small offset to properly start at 5:00 and not 4:59
 	for i in range(0, player_count):
 		$Tallies.text += "Player {num}'s troops: {troops}\n".format({"num":i, "troops":troop_count})
 
 func _process(_delta):
-	$TurnCounterTexture/TurnCountdown.text = str(int($TurnTimer.get_time_left())/60)+":"+str(int($TurnTimer.get_time_left())%60) # div by 60 to get minutes, mod by 60 to get mins
+	$TurnCounterTexture/TurnCountdown.text = str($TurnTimer.get_time_left()/60)+":"+str(int($TurnTimer.get_time_left())%60) # div by 60 to get minutes, mod by 60 to get mins
 
 func end_turn():
 	$TurnTimer.stop() 
 	# TODO: Switch player
-	$TurnTimer.start(5*60)
+	$TurnTimer.start(5*60+0.5)
 
 func _on_button_pressed():
 	end_turn()

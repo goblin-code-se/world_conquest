6ddf88402c217cf0edef93526e554006ebcbb5d7
0cr0wbar0 <finnianholland2@gmail.com>
2024-02-28 15:33:15 +0000

    The UI now works! :3

--- ui.gd
+++ ui.gd
@@ -4,15 +4,17 @@ extends CanvasLayer
 @export var troop_count = 25
 
 func _ready():
-	$TurnTimer.start(500) # Five minutes max for a turn
+	$TurnTimer.start(5*60) # Five minutes max for a turn
 	for i in range(0, player_count):
 		$Tallies.text += "Player {num}'s troops: {troops}\n".format({"num":i, "troops":troop_count})
 
 func _process(_delta):
-	$TurnCounterTexture.get_child(0).text = str($TurnTimer.get_time_left()/60)+":"+str($TurnTimer.get_time_left()%60.0) # div by 60 to get minutes, mod by 60 to get mins
+	$TurnCounterTexture/TurnCountdown.text = str(int($TurnTimer.get_time_left())/60)+":"+str(int($TurnTimer.get_time_left())%60) # div by 60 to get minutes, mod by 60 to get mins
 
 func end_turn():
-	$TurnTimer.stop()
+	$TurnTimer.stop() 
+	# TODO: Switch player
+	$TurnTimer.start(5*60)
 
 func _on_button_pressed():
 	end_turn()

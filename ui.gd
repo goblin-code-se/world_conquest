973a3ea234b9174f00f0e2a7071828ee06acd13d
0cr0wbar0 <finnianholland2@gmail.com>
2024-02-29 10:13:00 +0000

    Further tweaked UI logic to fix more bugs Player count is now no longer zero-indexed Added further formatting logic: e.g. four minutes and nine seconds on a timer now appears as "4:09" instead of "4:9" Added proper explanation of function that displays timer information on every frame

--- ui.gd
+++ ui.gd
@@ -5,14 +5,20 @@ extends CanvasLayer
 
 func _ready():
 	$TurnTimer.start(5*60+0.5) # Five minutes max for a turn, with small offset to properly start at 5:00 and not 4:59
-	for i in range(0, player_count):
+	for i in range(1, player_count+1):
 		$Tallies.text += "Player {num}'s troops: {troops}\n".format({"num":i, "troops":troop_count})
 
-func _process(_delta): # checks if exactly to a minute, if so, then adds extra zero so that "5:0" is corrected to "5:00"
+# hoo boy, explanation time
+# div by 60 to get minutes, mod by 60 to get seconds
+# first, checks if exactly to a minute, if so, then adds extra zero so that "5:0" is corrected to "5:00"
+# second, checks if seconds remaining to a minute is less than 10, if so, then adds extra zero in another place such that "4:9" is corrected to "4:09"
+func _process(_delta): 
 	if int($TurnTimer.get_time_left()) % 60 == 0:
 		$TurnCounterTexture/TurnCountdown.text = str(int($TurnTimer.get_time_left())/60)+":"+str(int($TurnTimer.get_time_left())%60)+"0"
-	else:
-		$TurnCounterTexture/TurnCountdown.text = str(int($TurnTimer.get_time_left())/60)+":"+str(int($TurnTimer.get_time_left())%60) # div by 60 to get minutes, mod by 60 to get seconds
+	elif int($TurnTimer.get_time_left()) % 60 < 10:
+		$TurnCounterTexture/TurnCountdown.text = str(int($TurnTimer.get_time_left())/60)+":0"+str(int($TurnTimer.get_time_left())%60) 
+	else: 
+		$TurnCounterTexture/TurnCountdown.text = str(int($TurnTimer.get_time_left())/60)+":"+str(int($TurnTimer.get_time_left())%60)
 
 func end_turn():
 	$TurnTimer.stop() 

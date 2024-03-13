b3ad2801c835f4c8540fbe17293a29a3c83cfad4
Charlie <mo436@sussex.ac.uk>
2024-03-13 13:38:29 +0000

    started making scene into a useable component

--- scripts/diceAngle.gd
+++ scripts/diceAngle.gd
@@ -1,16 +1,10 @@
 extends RigidBody3D
 @export var rotation_power: float = 10.0
-@export var rotation_minimum: float = 2.0
 @onready var result: int
-# Returns random float between +-rotation_minimum and +-rotation_power
-func random_rotation():
-	randomize()
-	return randf_range(rotation_minimum,rotation_power) * [1,-1].pick_random()
 
 
-#compares the rotation to the global 'up' and returns the value of the side that is pointed up
-#no more collision checks
-func find_orientation_value():
+#
+func find_orientation_value():	#compares the rotation to the global 'up' and returns the value of the side that is pointed up
 	var basis = transform.basis
 	for vector_value in [[basis.x.normalized(),5],[basis.y.normalized(),6],[basis.z.normalized(),4]]:
 		if (0.9< vector_value[0].dot(Vector3(0,1,0)) and vector_value[0].dot(Vector3(0,1,0)) <1.1):
@@ -21,10 +15,5 @@ func find_orientation_value():
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_angular_velocity(Vector3(random_rotation(),random_rotation(),random_rotation()))
-	
-	while (get_angular_velocity().length() > 0.1):
-		await get_tree().create_timer(0.2).timeout
-	
-	result = find_orientation_value()
-	print(result)
+	transform.basis = transform.basis.rotated(Vector3(randf_range(-1,1), randf_range(-1,1), randf_range(-1,1)).normalized(), randf_range(-PI,PI))
+	set_axis_velocity(Vector3(0,0,2-randf()))
--- scripts/dice_scene.gd
+++ scripts/dice_scene.gd
@@ -1,15 +1,4 @@
 extends Node3D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	$Camera3D.look_at($Dice.global_transform.origin, Vector3(0,1,0)) #.origin gives the origin. Vector3 to know what side is up
-
 func _input(event):
 	if Input.is_mouse_button_pressed(1):
 		get_tree().reload_current_scene()

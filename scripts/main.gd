25f7341275ee577e54e63385ada826e83087c095
John Cory-Wright <johncw004@gmail.com>
2024-03-21 17:04:42 +0000

    added moving troops functionality and added simple state changing functionality via button press

--- scripts/main.gd
+++ scripts/main.gd
@@ -7,13 +7,16 @@ var defender_territory
 var adjacent_territories
 var board_graph: Graph
 var edges: Array
+var moving_from: Territory
+var moving_to: Territory
 
 enum GameState {
 	SELECT_ATTACKER,
 	SELECT_ATTACKED,
 	ADDING_TROOPS,
 	ATTACK,
-	MOVING_TROOPS
+	MOVING_FROM,
+	MOVING_TO
 }
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -75,39 +78,80 @@ func _on_ui_change_state() -> void:
 			current_game_state = GameState.SELECT_ATTACKER
 			$Ui.update_game_state("SELECT_ATTACKER")
 		GameState.SELECT_ATTACKER:
-			current_game_state = GameState.MOVING_TROOPS
+			current_game_state = GameState.MOVING_FROM
 			$Ui.update_game_state("MOVING_TROOPS")
-		GameState.MOVING_TROOPS:
+		GameState.MOVING_FROM:
 			current_game_state = GameState.ADDING_TROOPS
 			$Ui.update_game_state("ADDING_TROOPS")
 
 
 func _on_board_main_territory_clicked(which: Territory):
-		match current_game_state:
-			GameState.ADDING_TROOPS:
-				print("adding troops")
-				if which.get_ownership() == current_player:
-					which.increment_troops(1) # can add multiple at a time eventually to help with turns of huge amounts of troops
-			GameState.SELECT_ATTACKER:
-				if which.get_ownership() == current_player: # This 0 has to be changed to player id
-					attacking_territory = which
-					print("ATTACKer territory: ", which.get_id())
-					current_game_state = GameState.SELECT_ATTACKED
+	match current_game_state:
+		GameState.ADDING_TROOPS:
+			print("adding troops")
+			if which.get_ownership() == current_player:
+				which.increment_troops(1) # can add multiple at a time eventually to help with turns of huge amounts of troops
+		GameState.SELECT_ATTACKER:
+			if which.get_ownership() == current_player: # This 0 has to be changed to player id
+				attacking_territory = which
+				print("ATTACKer territory: ", which.get_id())
+				current_game_state = GameState.SELECT_ATTACKED
+			else:
+				print("not your territory bro")
+		GameState.SELECT_ATTACKED:
+			var adjacent: bool = which.get_id() in board_graph.get_adjacent_nodes(attacking_territory.get_id())
+			if adjacent and which.get_ownership() != current_player: #and which.get_ownership() != attacking_territory.get_ownership():
+				defender_territory = which
+				current_game_state = GameState.SELECT_ATTACKED
+				print("defender: ", which.get_id())
+				current_game_state = GameState.SELECT_ATTACKER
+			else:
+				print("can't attackkkkk")
+		GameState.MOVING_FROM:
+			if which.get_ownership() == current_player:
+				moving_from = which
+				current_game_state = GameState.MOVING_TO
+			else:
+				print("not your territory")
+		GameState.MOVING_TO:
+			if which.get_ownership() == current_player:
+				moving_to = which
+				if valid_move(moving_from, moving_to):
+					print("valid movement")
+					move(moving_from, moving_to, 1)
+					current_game_state =  GameState.MOVING_FROM
 				else:
-					print("not your territory bro")
-			GameState.SELECT_ATTACKED:
-				var adjacent: bool = which.get_id() in board_graph.get_adjacent_nodes(attacking_territory.get_id())
-				if adjacent and which.get_ownership() != current_player: #and which.get_ownership() != attacking_territory.get_ownership():
-					defender_territory = which
-					current_game_state = GameState.SELECT_ATTACKED
-					print("defender: ", which.get_id())
-				else:
-					print("can't ATTACKkkkk")
+					print("not reachable")
+			else:
+				print("not your territory")
+				
 
 """John!"""
+"""
+checks if every node in the board belongs to one player id (game over)
+ - returns -1 if no winner
+ - returns player id of winner otherwise
+"""
 func is_game_over() -> int:
 	var winner = board_graph.get_nodes()[0].get_ownership()
 	for node in board_graph.get_nodes():
 		if node.get_ownership() != winner:
 			return -1
 	return winner
+	
+"""
+returns whether or not a move to and from a territory is valid
+ - returns true if both territories are connected and belong to same player
+ - returns false otherwise
+"""
+func valid_move(from: Territory, to: Territory) -> bool:
+	var connected_nodes = board_graph.dfs(from.get_id())
+	if to.get_ownership() == current_player and to.get_id() in connected_nodes:
+		return true
+	return false
+
+func move(from: Territory, to: Territory, amount: int):
+	from.increment_troops(-amount)
+	to.increment_troops(amount)
+	
+	

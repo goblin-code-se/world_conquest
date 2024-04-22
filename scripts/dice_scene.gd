ca84c1742595ac2468177733dcfa4ad8d4b78797
j0 <me@j0.lol>
2024-04-22 14:22:03 +0100

    Makes scene spawn multiple dice, change color, show results

--- scripts/dice_scene.gd
+++ scripts/dice_scene.gd
@@ -1,15 +1,46 @@
 extends Node3D
 
+var attacking_die = false;
+var count = 3;
+var internal_results = [];
+signal results;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	for i in range(count):
+		add_die()
+		
+	show_result(await results)
 
+func add_die():
+	var die;
+	if attacking_die:
+		die = preload("res://scenes/red_die.tscn").instantiate()
+	else:
+		die = preload("res://scenes/blue_die.tscn").instantiate()
+	
+	die.position = Vector3(randf()/3, 1, randf()/3)
+	add_child(die)
+	
+	# Add watcher functions to each die
+	die.result.connect(add_result.bind())
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	$Camera3D.look_at($Dice.global_transform.origin, Vector3(0,1,0)) #.origin gives the origin. Vector3 to know what side is up
+func show_result(results):
+	var string = "";
+	results.sort()
+	results.reverse()
+	for result in results:
+		string += str(result) + " "
+	
+	$Results.text = string.rstrip(" ")
+	if attacking_die:
+		$Results.modulate = Color(231.0/255.0, 0.0, 34.0 /255.0, 1.0)
+	else:
+		$Results.modulate = Color(0.0, 113.0/255.0, 254.0/255.0, 1.0)
+	$Results.visible = true
 
-func _input(event):
-	if Input.is_mouse_button_pressed(1):
-		get_tree().reload_current_scene()
+# Runs when die has finished rolling
+func add_result(result):
+	internal_results.append(result)
+	if internal_results.size() == count:
+		results.emit(internal_results)

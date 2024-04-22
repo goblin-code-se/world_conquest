713aee64110a926038986ca3695e525bd09dde89
j0 <me@j0.lol>
2024-04-22 15:20:58 +0100

    Remove duplicate files & enforce consistent naming scheme

--- scripts/die_angle.gd
+++ scripts/die_angle.gd
@@ -1,12 +1,12 @@
 extends RigidBody3D
 @export var rotation_power: float = 10.0
 @export var rotation_minimum: float = 2.0
-@onready var result: int
+signal result;
+
 # Returns random float between +-rotation_minimum and +-rotation_power
 func random_rotation():
 	randomize()
 	return randf_range(rotation_minimum,rotation_power) * [1,-1].pick_random()
-
 
 #compares the rotation to the global 'up' and returns the value of the side that is pointed up
 #no more collision checks
@@ -18,7 +18,6 @@ func find_orientation_value():
 		elif (-0.9> vector_value[0].dot(Vector3(0,1,0)) and vector_value[0].dot(Vector3(0,1,0)) >-1.1):
 			return(7-vector_value[1])
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_angular_velocity(Vector3(random_rotation(),random_rotation(),random_rotation()))
@@ -26,5 +25,5 @@ func _ready():
 	while (get_angular_velocity().length() > 0.1):
 		await get_tree().create_timer(0.2).timeout
 	
-	result = find_orientation_value()
-#	print(result)
+	# Returns die value in signal
+	result.emit(find_orientation_value())
